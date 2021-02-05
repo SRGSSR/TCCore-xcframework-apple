ee386acf7ea52df8aa9d1033877effb719fd520f
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2021-02-05 18:38:55 +0100

    Release srg4+b1 with Apple M1 simulator support

--- Package.swift
+++ Package.swift
@@ -3,14 +3,14 @@
 import PackageDescription
 
 struct PackageMetadata {
-    static let version: String = "4.5.4-srg3"
-    static let checksum: String = "903f5adc7fea6b5d3015734feba9770be2616b0b6732c5d8c16163af6007fac1"
+    static let version: String = "4.5.4-srg4+b1"
+    static let checksum: String = "cf16f4855ae4cd5ca32b00658ce4b99a5aee755b83d85f26c353a770fdd356d1"
 }
 
 let package = Package(
     name: "TCCore",
     platforms: [
-        .iOS(.v8),
+        .iOS(.v9),
         .tvOS(.v9)
     ],
     products: [
